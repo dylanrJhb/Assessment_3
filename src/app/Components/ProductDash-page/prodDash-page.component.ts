f7c2feac956c652673a17dd2cce4064622d058fc
dylanrJhb <106984903+dylanrJhb@users.noreply.github.com>
2022-06-07 10:34:59 +0200

    Add files via upload

---
+++ src/app/Components/ProductDash-page/prodDash-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-identification-page',
+  templateUrl: './prodDash-page.component.html',
+  styleUrls: ['./prodDash-page.component.scss']
+})
+export class prodDashPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/Components/Register-page/register-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-identification-page',
+  templateUrl: './register-page.component.html',
+  styleUrls: ['./register-page.component.scss']
+})
+export class regPageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
